9725ea48edef251a075a3e17ece1c523c0eaf5e5
Sebuuma Daniel <sedzsoft@gmail.com>
2015-02-20 18:59:12 +0300

    [Refactoring] Extracting the repetitive Calculator object generation.

--- tests/CalculatorTests.java
+++ tests/CalculatorTests.java
@@ -4,27 +4,25 @@ import static junit.framework.Assert.assertEquals;
 
 public class CalculatorTests {
 
+    private Calculator calculator = new Calculator();
+
     @Test
     public void testAdd(){
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.add(1,2));
     }
 
     @Test
     public void testSubtraction(){
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.subtract(5,2));
     }
 
     @Test
     public void testMultiplication(){
-        Calculator calculator = new Calculator();
         assertEquals(10, calculator.multiply(2,5));
     }
 
     @Test
     public void testDivision(){
-        Calculator calculator = new Calculator();
         assertEquals(5, calculator.divide(10,2));
     }
 
