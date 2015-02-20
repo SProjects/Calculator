34d4e9ba5c1fec6e1b6d266fe5afd5efa3d2f64c
Sebuuma Daniel <sedzsoft@gmail.com>
2015-02-20 17:54:03 +0300

    Adding Main and Calculator source files

---
+++ src/com/bluesquare/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.bluesquare.calculator;
+
+public class Calculator {
+
+    public int add(int value1, int value2) {
+        return (value1 + value2);
+    }
+
+}
---
+++ src/com/bluesquare/calculator/Main.java
@@ -0,0 +1,8 @@
+package com.bluesquare.calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
